05f61e1c37fcd23ee4da5ca19d109c9aab855920
JaninduWeerakkody <janindu.20230797@iit.ac.lk>
2024-12-04 15:12:28 +0530

    init

---
+++ ticket-system-frontend/src/App.js
@@ -0,0 +1,11 @@
+import ConfigurationForm from "./component/ConfigurationForm";
+
+function App() {
+  return (
+    <div >
+        <ConfigurationForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ ticket-system-frontend/src/component/ConfigurationForm.js
@@ -0,0 +1,11 @@
+import react from react;
+
+const ConfigurationForm = () => {
+    return (
+        <div>
+            <h1>Configuration Form</h1>
+        </div>
+    );
+}
+
+export default ConfigurationForm;
